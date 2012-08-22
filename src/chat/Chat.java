c1b653cff287e007cd0dc8fb0b9db46ac70761e4
xu <xu@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-08-22 07:39:53 +0000

    git-svn-id: svn://127.0.0.1/var/cvs/xu/Chatty/trunk@58 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -3,20 +3,20 @@ package chat;
 import java.util.ArrayList;
 
 public class Chat {
-	private String myMessage;
-	private ArrayList<String> history;
-	public String getMyMessage() {
-		return myMessage;
-	}
-	public void setMyMessage(String myMessage) {
-		this.myMessage = myMessage;
-	}
-	public ArrayList<String> getHistory() {
-		return history;
-	}
-	public void addToHistory(String msg) {
-		history.add(msg);
-	}
+//	private String myMessage;
+//	private ArrayList<String> history;
+//	public String getMyMessage() {
+//		return myMessage;
+//	}
+//	public void setMyMessage(String myMessage) {
+//		this.myMessage = myMessage;
+//	}
+//	public ArrayList<String> getHistory() {
+//		return history;
+//	}
+//	public void addToHistory(String msg) {
+//		history.add(msg);
+//	}
 	
 //	history.add(txtFiled.getText());
 //	txtrHistory.setText(history.get(0));
--- src/chat/ChatFrame.java
+++ src/chat/ChatFrame.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ChatFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -20,6 +29,7 @@ public class ChatFrame extends JFrame {
 				try {
 					ChatFrame frame = new ChatFrame();
 					frame.setVisible(true);
+					frame.textField.setText("Hi wrld!");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,8 +45,48 @@ public class ChatFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 12;
+		gbc_textArea.gridheight = 6;
+		gbc_textArea.insets = new Insets(0, 0, 5, 5);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 1;
+		contentPane.add(textArea, gbc_textArea);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 12;
+		gbc_textField.gridheight = 2;
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 7;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+//				textArea.setText(textField.getText());
+				
+			}
+		});
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.gridwidth = 2;
+		gbc_btnSend.gridx = 12;
+		gbc_btnSend.gridy = 8;
+		contentPane.add(btnSend, gbc_btnSend);
 	}
 
 }
